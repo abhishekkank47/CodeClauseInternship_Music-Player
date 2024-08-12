0872cbd1105ed015c6fdd3a552416c5838505328
abhishekkank47 <128067957+abhishekkank47@users.noreply.github.com>
2024-08-12 20:18:49 +0530

    Music Player UI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SongsData from "./Data/SongsData.json";
+import MusicPlayerComp from "./components/MusicPlayerComp";
+
+function App() {
+  return (
+    <>
+      <MusicPlayerComp />
+    </>
+  );
+}
+
+export default App;
